65f064a2877263edf7de21b6bc7aed2fdc4847e3
Kunal Sharma <kunal.iksm09@gmail.com>
2022-08-03 17:08:29 +0530

    added play prev or next song functionality

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const audio = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+// Music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-4",
+    displayName: "Front Row (Remix)",
+    artist: "Jacinto Design",
+  },
+];
 
 // Check if playing
 let isPlaying = false;
@@ -26,3 +53,41 @@ const pauseAudio = () => {
 playBtn.addEventListener("click", () =>
   isPlaying ? pauseAudio() : playAudio()
 );
+
+// Update DOM
+const loadSong = (song) => {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  audio.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+};
+
+// Current Song
+let audioIndex = 0;
+
+// Previous Audio
+const playPrev = () => {
+  audioIndex--;
+  if (audioIndex < 0) {
+    audioIndex = songs.length - 1;
+  }
+  loadSong(songs[audioIndex]);
+  playAudio();
+};
+
+// Next Audio
+const playNext = () => {
+  audioIndex++;
+  if (audioIndex > songs.length - 1) {
+    audioIndex = 0;
+  }
+  loadSong(songs[audioIndex]);
+  playAudio();
+};
+
+// On Load
+loadSong(songs[audioIndex]);
+
+// Event Listeners
+prevBtn.addEventListener("click", playPrev);
+nextBtn.addEventListener("click", playNext);
